15f88e059dad60804687c279642626e429ac2dcd
Kathryn Carr & Andy Uppendahl <student@epicodus.com>
2015-01-27 10:34:59 -0800

    Add functionality for index html page.

--- app.rb
+++ app.rb
@@ -4,3 +4,16 @@ also_reload("lib/**/*.rb")
 require("sinatra/activerecord")
 require("./lib/product")
 require("pg")
+
+get("/") do
+  @products = Product.all()
+  erb(:index)
+end
+
+post("/products") do
+  name = params.fetch("product_name")
+  price = params.fetch("product_price").to_f()
+  Product.create({:name => name, :price => price})
+  @products = Product.all()
+  erb(:index)
+end
