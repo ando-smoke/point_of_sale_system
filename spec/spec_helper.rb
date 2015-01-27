aa49d1f341c7c5b15d78e7d1981f6e98dd1e5d64
Kathryn Carr & Andy Uppendahl <student@epicodus.com>
2015-01-27 11:28:44 -0800

    Add files related to new Purchase class definition.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,16 @@ require("rspec")
 require("pg")
 require("sinatra/activerecord")
 require("product")
+require("purchase")
 
 RSpec.configure do |config|
   config.after(:each) do
     Product.all().each() do |product|
       product.destroy()
+    end
+
+    Purchase.all().each() do |purchase|
+      purchase.destroy()
     end
   end
 end
