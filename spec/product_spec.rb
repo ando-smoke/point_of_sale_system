10d9d37115f0510f5b84191412f5007783fd2480
Kathryn Carr & Andy Uppendahl <student@epicodus.com>
2015-01-27 11:36:41 -0800

    Test 'purchase' method of Product class.

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,5 +1,10 @@
 require("spec_helper")
 
 describe(Product) do
-
+  it("returns the purchase it belongs to") do
+    purchase = Purchase.create()
+    product = Product.create({ :name => "Bicycle",
+      :price => 350.00, :purchase_id => purchase.id() })
+    expect(product.purchase()).to(eq(purchase))
+  end
 end
