cfb366a1ab6d4be3b220990eb4ae2e8e98e3a764
Kathryn Carr & Andy Uppendahl <student@epicodus.com>
2015-01-27 11:45:44 -0800

    Test the products method in Purchase class

--- spec/purchase_spec.rb
+++ spec/purchase_spec.rb
@@ -1,5 +1,11 @@
 require("spec_helper")
 
 describe(Purchase) do
+  it("will return an array of product objects that belong to itself") do
+    test_purchase = Purchase.create()
+    test_product = Product.create({:name => "Kid bike",:price => 50.00, :purchase_id => test_purchase.id()})
+    test_product2 = Product.create({:name => "Coat", :price => 65.25,:purchase_id => test_purchase.id()})
+    expect(test_purchase.products()).to(eq([test_product, test_product2]))
+  end
 
 end
