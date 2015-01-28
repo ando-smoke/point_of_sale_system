84eb1b8c4f2f7745383be70d87f9593985ae124c
Kathryn Carr & Andy Uppendahl <student@epicodus.com>
2015-01-27 16:31:32 -0800

    Change db schema to reflect new purchase_date column in purchases db.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127192126) do
+ActiveRecord::Schema.define(version: 20150128002754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(version: 20150127192126) do
   end
 
   create_table "purchases", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.datetime "purchase_date"
   end
 
 end
