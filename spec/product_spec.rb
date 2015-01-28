43fc5ad92c13368f6205d2653845911e17a45f3a
Kathryn Carr & Andy Uppendahl <student@epicodus.com>
2015-01-27 16:36:22 -0800

    Add 'purchase_date' initialization variable to all new instances of Purchase class in spec files.

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -2,7 +2,7 @@ require("spec_helper")
 
 describe(Product) do
   it("returns the purchase it belongs to") do
-    purchase = Purchase.create()
+    purchase = Purchase.create({ :purchase_date => "2015-01-25" })
     product = Product.create({ :name => "Bicycle",
       :price => 350.00, :purchase_id => purchase.id() })
     expect(product.purchase()).to(eq(purchase))
--- spec/purchase_spec.rb
+++ spec/purchase_spec.rb
@@ -2,7 +2,7 @@ require("spec_helper")
 
 describe(Purchase) do
   it("will return an array of product objects that belong to itself") do
-    test_purchase = Purchase.create()
+    test_purchase = Purchase.create({ :purchase_date => "2015-01-25" })
     test_product = Product.create({:name => "Kid bike",:price => 50.00, :purchase_id => test_purchase.id()})
     test_product2 = Product.create({:name => "Coat", :price => 65.25,:purchase_id => test_purchase.id()})
     expect(test_purchase.products()).to(eq([test_product, test_product2]))
@@ -10,10 +10,10 @@ describe(Purchase) do
 
   describe("#total_cost") do
     it("will return the total of all products in the purchase") do
-      test_purchase = Purchase.create()
+      test_purchase = Purchase.create({ :purchase_date => "2015-01-25" })
       test_product = Product.create({:name => "Kid bike",:price => 50.00, :purchase_id => test_purchase.id()})
       test_product2 = Product.create({:name => "Coat", :price => 65.25,:purchase_id => test_purchase.id()})
-      expect(test_purchase.total_cost()).to(eq(115.25))    
+      expect(test_purchase.total_cost()).to(eq(115.25))
     end
   end
 
